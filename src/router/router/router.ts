c7097b34151f84f0eeb23321589f8ff5a36bb340
nanjingtao <nanjingtao@tal>
2022-09-21 19:23:44 +0800

    dev: 初始化vite仓库

--- src/router/router/router.ts
+++ src/router/router/router.ts
@@ -4,10 +4,10 @@
  * alwaysShow: true // 将始终显示根菜单
  * */
 
-import type { RouteRecordRaw } from "vue-router"
+import type { RouteRecordRaw } from 'vue-router'
 
-import asyncRoutes from "./asyncRoutes"
-import constantRoutes from "./constantRoutes"
+import asyncRoutes from './asyncRoutes'
+import constantRoutes from './constantRoutes'
 
 /**
  * 动态路由(权限控制)
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vue from "@vitejs/plugin-vue"
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons"
 import UnoCSS from "unocss/vite"
 
-/** 配置项文档：https://cn.vitejs.dev/config */
 export default (configEnv: ConfigEnv): UserConfigExport => {
 
   const viteEnv = loadEnv(configEnv.mode, process.cwd()) as ImportMetaEnv
@@ -59,23 +58,6 @@ export default (configEnv: ConfigEnv): UserConfigExport => {
         symbolId: "icon-[dir]-[name]"
       }),
       UnoCSS()
-      /** 自动按需引入 (已更改为完整引入，所以注释了) */
-      // AutoImport({
-      //   dts: "./types/auto-imports.d.ts",
-      //   /** 自动按需导入 Element-Plus 相关函数，比如 ElMessage */
-      //   resolvers: [ElementPlusResolver()],
-      //   /** 根据自动按需导入的相关 API，生成 .eslintrc-auto-import.json 文件供 Eslint 识别 */
-      //   eslintrc: {
-      //     enabled: true, // 默认 false
-      //     filepath: "./types/.eslintrc-auto-import.json", // 默认 "./.eslintrc-auto-import.json"
-      //     globalsPropValue: true // 默认 true (true | false | "readonly" | "readable" | "writable" | "writeable")
-      //   }
-      // }),
-      // Components({
-      //   dts: "./types/components.d.ts",
-      //   /** 自动按需导入 Element-Plus 组件 */
-      //   resolvers: [ElementPlusResolver()]
-      // })
     ]
   }
 }
