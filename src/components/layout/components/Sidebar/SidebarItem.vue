132b47fc23d1899ce8e32a07d65e73f68458b698
nanjingtao <nanjingtao@tal>
2022-11-23 15:09:59 +0800

    dev: 初始化前端vite最新框架

--- src/components/layout/components/Sidebar/SidebarItem.vue
+++ src/components/layout/components/Sidebar/SidebarItem.vue
@@ -1,3 +1,37 @@
+<template>
+  <div
+    v-if="!props.item.meta?.hidden"
+    :class="{ 'simple-mode': props.isCollapse, 'first-level': props.isFirstLevel }"
+  >
+    <template v-if="!alwaysShowRootMenu && theOnlyOneChild && !theOnlyOneChild.children">
+      <SidebarItemLink v-if="theOnlyOneChild.meta" :to="resolvePath(theOnlyOneChild.path)">
+        <el-menu-item :index="resolvePath(theOnlyOneChild.path)">
+          <svg-icon v-if="theOnlyOneChild.meta.icon" :name="theOnlyOneChild.meta.icon" />
+          <template v-if="theOnlyOneChild.meta.title" #title>
+            {{ theOnlyOneChild.meta.title }}
+          </template>
+        </el-menu-item>
+      </SidebarItemLink>
+    </template>
+    <el-sub-menu v-else :index="resolvePath(props.item.path)" popper-append-to-body>
+      <template #title>
+        <svg-icon v-if="props.item.meta && props.item.meta.icon" :name="props.item.meta.icon" />
+        <span v-if="props.item.meta && props.item.meta.title">{{ props.item.meta.title }}</span>
+      </template>
+      <template v-if="props.item.children">
+        <sidebar-item
+          v-for="child in props.item.children"
+          :key="child.path"
+          :item="child"
+          :is-collapse="props.isCollapse"
+          :is-first-level="false"
+          :base-path="resolvePath(child.path)"
+        />
+      </template>
+    </el-sub-menu>
+  </div>
+</template>
+
 <script lang="ts" setup>
 import { computed } from 'vue'
 import type { PropType } from 'vue'
@@ -50,8 +84,6 @@ const theOnlyOneChild = computed(() => {
       }
     }
   }
-  // If there is no children, return itself with path removed,
-  // because this.basePath already contains item's path information
   return { ...props.item, path: '' }
 })
 
@@ -66,40 +98,6 @@ const resolvePath = (routePath: string) => {
 }
 </script>
 
-<template>
-  <div
-    v-if="!props.item.meta?.hidden"
-    :class="{ 'simple-mode': props.isCollapse, 'first-level': props.isFirstLevel }"
-  >
-    <template v-if="!alwaysShowRootMenu && theOnlyOneChild && !theOnlyOneChild.children">
-      <SidebarItemLink v-if="theOnlyOneChild.meta" :to="resolvePath(theOnlyOneChild.path)">
-        <el-menu-item :index="resolvePath(theOnlyOneChild.path)">
-          <svg-icon v-if="theOnlyOneChild.meta.icon" :name="theOnlyOneChild.meta.icon" />
-          <template v-if="theOnlyOneChild.meta.title" #title>
-            {{ theOnlyOneChild.meta.title }}
-          </template>
-        </el-menu-item>
-      </SidebarItemLink>
-    </template>
-    <el-sub-menu v-else :index="resolvePath(props.item.path)" popper-append-to-body>
-      <template #title>
-        <svg-icon v-if="props.item.meta && props.item.meta.icon" :name="props.item.meta.icon" />
-        <span v-if="props.item.meta && props.item.meta.title">{{ props.item.meta.title }}</span>
-      </template>
-      <template v-if="props.item.children">
-        <sidebar-item
-          v-for="child in props.item.children"
-          :key="child.path"
-          :item="child"
-          :is-collapse="props.isCollapse"
-          :is-first-level="false"
-          :base-path="resolvePath(child.path)"
-        />
-      </template>
-    </el-sub-menu>
-  </div>
-</template>
-
 <style lang="scss" scoped>
 .svg-icon {
   margin-right: 20px;
@@ -113,6 +111,7 @@ const resolvePath = (routePath: string) => {
       .el-sub-menu__icon-arrow {
         display: none;
       }
+
       span {
         visibility: hidden;
       }
